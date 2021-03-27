0e8a2bb215cfade9a790fcfcbfd5ef51d262ae21
sakshamsangraula <sakshamsangraula45@gmail.com>
2021-03-27 15:12:29 -0500

    completed the blog project

--- blog-templating-start/blog-templating-start/main.py
+++ blog-templating-start/blog-templating-start/main.py
@@ -8,8 +8,17 @@ app = Flask(__name__)
 def home():
     all_posts = requests.get('https://api.npoint.io/5abcca6f4e39b4955965')
     all_posts = all_posts.json()
-    return render_template("index.html", all_posts = all_posts)
+    return render_template("index.html", posts=all_posts)
 
+# route the read links to respective blog posts
+@app.route('/post/<int:num>')
+def blog_posts(num):
+    all_posts = requests.get('https://api.npoint.io/5abcca6f4e39b4955965')
+    all_posts = all_posts.json()
+    for post in all_posts:
+        if post['id'] == num:
+            select_post = post
+    return render_template("post.html", post=select_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
